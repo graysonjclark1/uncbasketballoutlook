22bfaf596e31ce3a5f947a01bb328e2e1cadbe30
graysonjclark1 <gclark@unc.edu>
2026-02-03 15:18:42 -0500

    Update UI

--- docs/index.js
+++ docs/index.js
@@ -21,10 +21,26 @@ getResult().then(data =>  {
     element.innerHTML = JSON.stringify(data.prediction, null, 2).replace(/['"]+/g, '');
 })
 
+function getOrdinalSuffix(day) {
+  if (day >= 11 && day <= 13) return 'th';
+
+  switch (day % 10) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
 
 function updateTime() {
             const today = new Date();
-            document.getElementById('title').textContent += today.toDateString();
+            const monthFormatter = new Intl.DateTimeFormat('en-US', { month: 'long' });
+            const yearFormatter  = new Intl.DateTimeFormat('en-US', { year: 'numeric' });
+            const day = today.getDate();
+            const suffix = getOrdinalSuffix(day);
+            const formattedDate =`${monthFormatter.format(today)} ${day}${suffix}, ${yearFormatter.format(today)}`;
+            document.getElementById('title').textContent += formattedDate;
 }
 
 updateTime();
